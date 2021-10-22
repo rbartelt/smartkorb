b78ed9591c38029e4c12f70b2e4c951ff952e77f
rbartelt <robert_bartelt@web.de>
2021-10-22 18:53:06 +0200

    added RecyclerView to display a list of Körbe

---
+++ app/src/main/java/de/xxlstrandkorbverleih/smartkorb/KorbAdapter.java
@@ -0,0 +1,57 @@
+package de.xxlstrandkorbverleih.smartkorb;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class KorbAdapter extends RecyclerView.Adapter<KorbAdapter.KorbHolder> {
+    private List<Korb> körbe = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public KorbHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.korb_item, parent, false);
+        return new KorbHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull KorbHolder holder, int position) {
+        Korb currentKorb = körbe.get(position);
+        holder.textViewType.setText(currentKorb.getType());
+        holder.textViewNumber.setText(String.valueOf(currentKorb.getNumber()));
+        holder.textViewLocation.setText(String.valueOf(currentKorb.getAccuracy())); //TODO: set updatetime
+    }
+
+    @Override
+    public int getItemCount() {
+        return körbe.size();
+    }
+
+    //insert körbe in ResyclerView
+    public void setKörbe(List<Korb> körbe) {
+        this.körbe = körbe;
+        notifyDataSetChanged();
+    }
+
+    class KorbHolder extends RecyclerView.ViewHolder {
+        private TextView textViewType;
+        private TextView textViewNumber;
+        private TextView textViewLocation;
+
+        public KorbHolder(View itemView) {
+            super(itemView);
+            textViewType = itemView.findViewById(R.id.text_view_type);
+            textViewNumber = itemView.findViewById(R.id.text_view_number);
+            textViewLocation = itemView.findViewById(R.id.text_view_location_set_on);
+        }
+    }
+}
--- app/src/main/java/de/xxlstrandkorbverleih/smartkorb/MainActivity.java
+++ app/src/main/java/de/xxlstrandkorbverleih/smartkorb/MainActivity.java
@@ -3,6 +3,8 @@ package de.xxlstrandkorbverleih.smartkorb;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -17,12 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        KorbAdapter adapter = new KorbAdapter();
+        recyclerView.setAdapter(adapter);
+
         korbViewModel = new ViewModelProvider(this).get(KorbViewModel.class);
         korbViewModel.getAllKörbe().observe(this, new Observer<List<Korb>>(){
 
             @Override
             public void onChanged(List<Korb> körbe) {
-                //update ResyclerView
+                adapter.setKörbe(körbe);
+
 
             }
         });
