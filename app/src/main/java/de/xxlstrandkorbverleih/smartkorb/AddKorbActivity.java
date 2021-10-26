8aa1799c157ca9afc59e7ad68e3e1aa3ced5a353
rbartelt <robert_bartelt@web.de>
2021-10-26 22:48:47 +0200

    Added the AddKorb Feature

---
+++ app/src/main/java/de/xxlstrandkorbverleih/smartkorb/AddKorbActivity.java
@@ -0,0 +1,69 @@
+package de.xxlstrandkorbverleih.smartkorb;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddKorbActivity extends AppCompatActivity {
+    public static final String EXTRA_NUMBER = "de.xxlstrandkorbverleih.smartkorb.EXTRA_NUMBER";
+    public static final String EXTRA_TYPE = "de.xxlstrandkorbverleih.smartkorb.EXTRA_TYPE";
+    private EditText editTextNumber;
+    private EditText editTextType;
+    private Button buttonGetLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_korb);
+
+        editTextNumber = findViewById(R.id.edit_text_number);
+        editTextType = findViewById(R.id.edit_text_type);
+        buttonGetLocation = findViewById(R.id.button_get_location);
+        //Todo : Add OnClickListener and implement Method to get Location
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Korb");
+    }
+
+    private void saveKorb() {
+        String type = editTextType.getText().toString();
+        String number = editTextNumber.getText().toString();
+        if(type.trim().isEmpty() || number.trim().isEmpty() ) {
+            Toast.makeText(this, "Please insert a Type and positiv Number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //Todo: Create ViewModel for AddKorbActivity (AddKorbViewModel) and do Databaseoperations with it. Then remove the next Section.
+        //In the Tutorial the Data were passed to MainActivity wich uses the KorbViewModel to do the Database operation.
+        Intent data = new Intent();
+        data.putExtra(EXTRA_NUMBER,number);
+        data.putExtra(EXTRA_TYPE,type);
+        setResult(RESULT_OK, data);
+        finish();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();          //get menuinflater
+        menuInflater.inflate(R.menu.add_korb_menu, menu);       //set the menu resource
+        return true;                                            //true displays the menu
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {                                     //witch item is selected
+            case R.id.save_korb:
+                saveKorb();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/de/xxlstrandkorbverleih/smartkorb/MainActivity.java
+++ app/src/main/java/de/xxlstrandkorbverleih/smartkorb/MainActivity.java
@@ -1,16 +1,23 @@
 package de.xxlstrandkorbverleih.smartkorb;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int ADD_KORB_REQUEST = 1;
 
     private KorbViewModel korbViewModel;
 
@@ -18,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton buttonAddKorb = findViewById(R.id.button_add_korb);
+        buttonAddKorb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddKorbActivity.class);
+                startActivityForResult(intent,ADD_KORB_REQUEST);
+            }
+        });
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -36,5 +52,22 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == ADD_KORB_REQUEST && resultCode == RESULT_OK) {
+            String type = data.getStringExtra(AddKorbActivity.EXTRA_TYPE);
+            int number = Integer.valueOf(data.getStringExtra(AddKorbActivity.EXTRA_NUMBER));
+            
+            Korb korb = new Korb(number, type, 1, 1, 1);
+            korbViewModel.insert(korb);
+
+            Toast.makeText(this, "Korb saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Korb not saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
