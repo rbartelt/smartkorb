dffa23d7b94a90293fff43021f837df7d4e82efd
= <=>
2022-02-07 20:18:22 +0100

    handle Nfc in addEditKorbFragment

--- app/src/main/java/de/xxlstrandkorbverleih/smartkorb/feature_korb/presentation/addEditKorb/addEditKorbFragment.java
+++ app/src/main/java/de/xxlstrandkorbverleih/smartkorb/feature_korb/presentation/addEditKorb/addEditKorbFragment.java
@@ -1,6 +1,8 @@
 package de.xxlstrandkorbverleih.smartkorb.feature_korb.presentation.addEditKorb;
 
 import android.content.Context;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -25,18 +27,24 @@ import androidx.navigation.Navigation;
 
 import de.xxlstrandkorbverleih.smartkorb.R;
 import de.xxlstrandkorbverleih.smartkorb.feature_korb.domain.model.Korb;
-import de.xxlstrandkorbverleih.smartkorb.feature_korb.presentation.common.NfcDialog;
 import de.xxlstrandkorbverleih.smartkorb.feature_korb.presentation.common.NfcDialogViewModel;
 import de.xxlstrandkorbverleih.smartkorb.feature_korb.presentation.showKoerbe.KorbViewModel;
 
-public class addEditKorbFragment extends Fragment {
+public class addEditKorbFragment extends Fragment implements NfcAdapter.ReaderCallback {
     private KorbViewModel korbViewModel;
-    private NfcDialogViewModel nfcDialogViewModel;
     private EditText editTextNumber;
     private TextView textViewKeyUid;
+    private TextView textViewKorbUid;
     private Spinner spinnerType;
     private Button buttonWriteKeyTag;
     private Button buttonWriteKorbTag;
+
+    private String strTagId;
+    private NfcAdapter mNfcAdapter;
+
+
+    private boolean uidKorb = false;
+    private boolean uidKey = false;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -58,6 +66,7 @@ public class addEditKorbFragment extends Fragment {
         View addEditKorbView = getView();
         editTextNumber = addEditKorbView.findViewById(R.id.edit_text_number);
         textViewKeyUid = addEditKorbView.findViewById(R.id.text_view_key_uid);
+        textViewKorbUid = addEditKorbView.findViewById(R.id.text_view_korb_uid);
         spinnerType = addEditKorbView.findViewById(R.id.edit_text_type);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.types, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -67,16 +76,14 @@ public class addEditKorbFragment extends Fragment {
         buttonWriteKeyTag = addEditKorbView.findViewById(R.id.button_write_key_tag);
         buttonWriteKeyTag.setOnClickListener(v -> {
             //TODO: Ensure UID is unique -> create Methode or Usecase Class in Domainmodel
-            NavDirections action = addEditKorbFragmentDirections.actionAddEditKorbFragmentToNfcDialog();
-            Navigation.findNavController(getView()).navigate(action);
+            enableKeyRead();
 
         });
         /*Handle Button Write Korb Tag*/
         buttonWriteKorbTag = addEditKorbView.findViewById(R.id.button_write_korb_tag);
         buttonWriteKorbTag.setOnClickListener(v-> {
             //TODO: Ensure UID is unique -> create Methode or Usecase Class in Domainmodel
-            NavDirections action = addEditKorbFragmentDirections.actionAddEditKorbFragmentToNfcDialog();
-            Navigation.findNavController(getView()).navigate(action);
+            enableKorbRead();
         });
 
         korbViewModel = new ViewModelProvider(requireActivity()).get(KorbViewModel.class);
@@ -84,12 +91,67 @@ public class addEditKorbFragment extends Fragment {
             if(korb!=null) {
                 editTextNumber.setText(String.valueOf(korb.getNumber()));
                 spinnerType.setSelection(adapter.getPosition(korb.getType()));
-                if(korb.getKeyUid()!=null)
-                    textViewKeyUid.setText(korb.getKeyUid());
+                textViewKeyUid.setText(korb.getKeyUid());
+                textViewKorbUid.setText(korb.getKorbUid());
             }
         });
-        //TODO: check if there is a better way to get the tag uid from the Dialog
-        nfcDialogViewModel = new ViewModelProvider(requireActivity()).get(NfcDialogViewModel.class);
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mNfcAdapter!=null){
+            disableReaderMode();
+        }
+    }
+
+    @Override
+    public void onTagDiscovered(Tag tag) {
+        //get TagId and convert to String
+        byte[] tag_id = tag.getId();
+        strTagId = new String();
+        for (int i = 0; i < tag_id.length; i++) {
+            String x = Integer.toHexString(((int) tag_id[i] & 0xff));
+            if (x.length() == 1) {
+                x = '0' + x;
+            }
+            strTagId += x + ' ';
+        }
+
+        // Success if got to here
+        String finalStrTagId = strTagId;
+        getActivity().runOnUiThread(() -> {
+            if(uidKorb)
+                textViewKorbUid.setText(strTagId);
+            if(uidKey)
+                textViewKeyUid.setText(strTagId);
+            Toast.makeText(getActivity(), finalStrTagId, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void enableReaderMode() {
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(getActivity());
+        if (mNfcAdapter != null) {
+            Bundle options = new Bundle();
+            // Work around for some broken Nfc firmware implementations that poll the card too fast
+            options.putInt(NfcAdapter.EXTRA_READER_PRESENCE_CHECK_DELAY, 250);
+
+            // Enable ReaderMode for all types of card and disable platform sounds
+            mNfcAdapter.enableReaderMode(getActivity(),
+                    this,
+                    NfcAdapter.FLAG_READER_NFC_A |
+                            NfcAdapter.FLAG_READER_NFC_B |
+                            NfcAdapter.FLAG_READER_NFC_F |
+                            NfcAdapter.FLAG_READER_NFC_V |
+                            NfcAdapter.FLAG_READER_NFC_BARCODE |
+                            NfcAdapter.FLAG_READER_NO_PLATFORM_SOUNDS,
+                    options);
+        }
+    }
+
+    private void disableReaderMode() {
+        if (mNfcAdapter != null) {
+            mNfcAdapter.disableReaderMode(getActivity());
+        }
     }
 
     @Override
@@ -99,7 +161,7 @@ public class addEditKorbFragment extends Fragment {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {                                     //witch item is selected
+        switch (item.getItemId()) {                                     //wich item is selected
             case R.id.save_korb:
                 if(saveKorb()) {
                     NavDirections action = addEditKorbFragmentDirections.actionAddEditKorbFragmentToShowKoerbeFragment();
@@ -125,20 +187,22 @@ public class addEditKorbFragment extends Fragment {
     private boolean saveKorb() {
         String type = spinnerType.getSelectedItem().toString();
         String number = editTextNumber.getText().toString();
+        String uidKorb = textViewKorbUid.getText().toString();
+        String uidKey = textViewKeyUid.getText().toString();
         if (type.trim().isEmpty() || number.trim().isEmpty()) {
             Toast.makeText(getContext(), "Please insert a Type and positiv Number", Toast.LENGTH_SHORT).show();
             return false;
         } else {
             //insert new korb
             if (korbViewModel.getSelectedKorb().getValue() == null) {
-                Korb korb = new Korb(Integer.parseInt(number), type, 1, 1, 1, nfcDialogViewModel.getUid(), null);
+                Korb korb = new Korb(Integer.parseInt(number), type, 1, 1, 1, uidKey, uidKorb);
                 korbViewModel.insert(korb);
                 Toast.makeText(getContext(), "Korb saved", Toast.LENGTH_SHORT).show();
                 return true;
             }
             //update existing korb
             else {
-                Korb korb = new Korb(Integer.valueOf(number), type, 1, 1, 1, nfcDialogViewModel.getUid(), null);
+                Korb korb = new Korb(Integer.valueOf(number), type, 1, 1, 1, uidKey, uidKorb);
                 korb.setId(korbViewModel.getSelectedKorb().getValue().getId());
                 korbViewModel.update(korb);
                 Toast.makeText(getContext(), "Korb updated", Toast.LENGTH_SHORT).show();
@@ -147,4 +211,18 @@ public class addEditKorbFragment extends Fragment {
         }
     }
 
+    private void enableKeyRead(){
+        uidKey = true;
+        uidKorb = false;
+        textViewKeyUid.setText("Approach Key Tag");
+        enableReaderMode();
+    }
+
+    private void enableKorbRead() {
+        uidKey = false;
+        uidKorb = true;
+        textViewKorbUid.setText("Approach Korb Tag");
+        enableReaderMode();
+
+    }
 }
