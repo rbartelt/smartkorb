2f8727594dcdced973e8a9d75ca5b9753ecc495c
rbartelt <robert_bartelt@web.de>
2022-03-06 19:43:05 +0100

    -changed fragment_booking.xml to constraintlayout -changed BookingFragment to Databinding -added BookingFragmentViewModel

--- app/src/main/java/de/xxlstrandkorbverleih/smartkorb/feature_korb/presentation/booking/BookingFragment.java
+++ app/src/main/java/de/xxlstrandkorbverleih/smartkorb/feature_korb/presentation/booking/BookingFragment.java
@@ -12,7 +12,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.util.Pair;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -26,23 +28,37 @@ import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
 
+import dagger.hilt.android.AndroidEntryPoint;
+import de.xxlstrandkorbverleih.smartkorb.BR;
 import de.xxlstrandkorbverleih.smartkorb.R;
+import de.xxlstrandkorbverleih.smartkorb.databinding.FragmentBookingBinding;
 
+@AndroidEntryPoint
 public class BookingFragment extends Fragment implements OnMapReadyCallback {
 
+    /////////////////////////////////////////////////////////////////////////////
+    //Membervariables
+    private FragmentBookingBinding binding;
+    private BookingFragmentViewModel viewModel;
     private TextView date;
     private MapView mMapView;
     private RecyclerView mKorbRecyclerView;
 
     public static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
+    //////////////////////////////////////////////////////////////////////////////
+    //Lifecyclemethods
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_booking, container, false);
-        mMapView = view.findViewById(R.id.korb_map);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_booking, container, false);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+        viewModel = new ViewModelProvider(this).get(BookingFragmentViewModel.class);
+        binding.setVariable(BR.viewmodel, viewModel);
+        mMapView = binding.getRoot().findViewById(R.id.korb_map);
         initGoogleMap(savedInstanceState);
-        return view;
+        return binding.getRoot();
     }
 
     @Override
@@ -50,21 +66,77 @@ public class BookingFragment extends Fragment implements OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState);
 
     }
-    private void initGoogleMap(Bundle savedInstanceState){
-        // *** IMPORTANT ***
-        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
-        // objects or sub-Bundles.
-        Bundle mapViewBundle = null;
-        if (savedInstanceState != null) {
-            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
-        }
 
-        mMapView.onCreate(mapViewBundle);
-
-        mMapView.getMapAsync(this);
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //Override Methods
+    @Override
+    public void onMapReady(GoogleMap map) {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //map.setMyLocationEnabled(true);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //Helper Methods
     private void initSpinnerAndCalendar() {
         /*LocalDateTime today = LocalDateTime.now();
 
@@ -90,69 +162,17 @@ public class BookingFragment extends Fragment implements OnMapReadyCallback {
          */
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
-        if (mapViewBundle == null) {
-            mapViewBundle = new Bundle();
-            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+    private void initGoogleMap(Bundle savedInstanceState) {
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
         }
 
-        mMapView.onSaveInstanceState(mapViewBundle);
-    }
+        mMapView.onCreate(mapViewBundle);
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mMapView.onResume();
+        mMapView.getMapAsync(this);
     }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mMapView.onStart();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        mMapView.onStop();
-    }
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-       //map.setMyLocationEnabled(true);
-    }
-
-    @Override
-    public void onPause() {
-        mMapView.onPause();
-        super.onPause();
-    }
-
-    @Override
-    public void onDestroy() {
-        mMapView.onDestroy();
-        super.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mMapView.onLowMemory();
-    }}
+}
---
+++ app/src/main/java/de/xxlstrandkorbverleih/smartkorb/feature_korb/presentation/booking/BookingFragmentViewModel.java
@@ -0,0 +1,6 @@
+package de.xxlstrandkorbverleih.smartkorb.feature_korb.presentation.booking;
+
+import androidx.lifecycle.ViewModel;
+
+public class BookingFragmentViewModel extends ViewModel {
+}
